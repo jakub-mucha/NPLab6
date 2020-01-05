3ebd092f85efb23fae27a14b4bd55da17c0291b0
unknown <gocaffe36@gmail.com>
2020-01-05 22:34:34 +0100

    Nowa funkcja wyswietlania kwadratu dlugosci wektora jednowymiarowego

--- test.h
+++ test.h
@@ -8,6 +8,10 @@ int dlugoscW2kwadrat(int x1, int x2, int y1, int y2)
 	return (x1*x2 + y1*y2);
 }
 
+int dlugoscW1kwadrat(int x, int y){
+	return x*y;
+}
+
 void wypiszDlugosc(int x1, int x2, int y1, int y2){
 	printf("%d", dlugoscW2kwadrat(x1,x2,y1,y2));
 }
