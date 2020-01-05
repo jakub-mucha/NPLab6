9289c8b3e64a5f9b964f0274b11720ddbd5454f1
Karol <karol-pyszka@users.noreply.github.com>
2020-01-05 13:55:38 +0100

    Add new feature to main.c

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 int main()
 {
 	double end,val = 60;
-	int var = 123, endpoint;
+	int var = 123, endpoint,twenty;
 	end = sinus(val);
 	printf(end,%d);
+	printf("/n");
 	endpoint = randomize(var)
 	printf(endpoint, %i);
+	printf("/n");
+	twenty = multiex(var,endpoint);
+	printf(twenty, %i);
+	printf("/n");
 	return 0;
 }
