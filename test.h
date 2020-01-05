272d30b842bd0344d6a7e88b4638e7ba7e13b833
unknown <gocaffe36@gmail.com>
2020-01-05 22:31:15 +0100

    Dodanie nowej funkcji długości wektora do kwadratu

--- test.h
+++ test.h
@@ -3,6 +3,15 @@ int dodajWektory(int x, int y)
 	return x+y;
 }
 
+int dlugoscW2kwadrat(int x1, int x2, int y1, int y2)
+{
+	return (x1*x2 + y1*y2);
+}
+
 void dodajWektory2Wymiarowe(int x1, int x2, int y1, int y2){
 	printf("[%d,%d]", x1+x2, y1+y2);
+}
+
+void wypiszWektor1(int x, int y){
+	printf("[%d]", dodajWektory(x,y));
 }
