347146297992eb004395de1b3886ea2851f9f5df
Matio1 <mateusz.nalazek@gmail.com>
2020-01-05 16:43:21 +0100

    Mnozenie wewnatrz funkcji main

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-    printf("Test dzialania");
+    int a, b;
+    printf("Test mnozenia\n");
+
+    printf("Podaj pierwsza liczbe\n");
+    scanf("%d", &a);
+
+    printf("Podaj druga liczbe\n");
+    scanf("%d", &b);
+
+    printf("Wynik mnozenia wynosi %d\n",a*b);
 
     return 0;
 }
