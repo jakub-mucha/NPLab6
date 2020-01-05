d8db41f7d5ef316b4f69aee5694acf34b8152332
Jakub Mucha <jakub.mucha23@outlook.com>
2020-01-05 09:40:58 +0100

    Dodano import dodawanie.c do main.c

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "dodawanie.h"
 
 int main()
 {
-    printf("Test dzialania");
+    printf("Test dzialania\n");
+    printf("7 + 15 = %d\n", dodajInt(7, 15));
 
     return 0;
 }
